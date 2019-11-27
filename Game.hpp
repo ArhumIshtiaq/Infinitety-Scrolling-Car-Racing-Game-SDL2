f65d47126f74ec5900b6ea65eb99c930b14e9eaa
Neha-Jafry <51692007+Neha-Jafry@users.noreply.github.com>
2019-11-27 21:08:02 +0500

    .

--- Game.hpp
+++ Game.hpp
@@ -6,8 +6,9 @@ class Game
 {
     public:
         bool isRunning;
-        SDL_Renderer* renderer;
-        SDL_Window* window;
+        // SDL_Renderer* renderer;
+        // SDL_Window* window;
+        SDL_Texture* stScreen;
         static int obj_count;
         void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
         void handleEvents();
--- Menu.hpp
+++ Menu.hpp
@@ -5,7 +5,20 @@
 class Menu
 {
     public:
-        SDL_Image* background;
+        SDL_Surface* background;
         void click(SDL_Renderer*);
-        void show(SDL_Image*);
+        void show(SDL_Surface*);
 };
+
+void Menu::show(SDL_Surface* screen)
+{
+    int x,y;
+    const int numMenu = 3;
+    const char* label[numMenu] = {"Start", "How to Play", "Exit"};
+    SDL_Surface* menus[numMenu];
+    bool selected[numMenu] = {0,0,0};
+    menus[0] = ;
+    menus[1] = ;
+    SDL_Rect pos[numMenu];
+    pos[0].x = screen->clip_rect.w/2 + menus[0]->clip.h/2;
+}
