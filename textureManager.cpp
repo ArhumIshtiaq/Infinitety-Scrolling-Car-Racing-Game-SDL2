ef37b5ef08c1bf6a3a515240c249dff6dec75f12
Neha-Jafry <51692007+Neha-Jafry@users.noreply.github.com>
2019-12-18 08:16:29 +0500

    config files and texture manager updated

--- textureManager.cpp
+++ textureManager.cpp
@@ -7,3 +7,19 @@ SDL_Texture *textureManager::Loadtexture(const char *filepath, SDL_Renderer *ren
     SDL_FreeSurface(temp);
     return tex;
 }
+
+SDL_Texture* textureManager::LoadtextureFromText(const char* textToRender, SDL_Color textColor, int textSize, SDL_Renderer* ren){
+    if( TTF_Init() == -1 )
+    {
+        printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() );
+    }
+
+    TTF_Font *font = TTF_OpenFont( "arial.ttf", textSize );
+    SDL_Surface *tempTextSurface = TTF_RenderText_Solid( font, textToRender, textColor);
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(ren,tempTextSurface);
+    SDL_FreeSurface(tempTextSurface);
+    return tex;
+}
+
+textureManager::~TextureManager(){
+};
--- textureManager.hpp
+++ textureManager.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "game.hpp"
+#include <SDL_ttf.h>
 
 class textureManager
 {
 public:
     static SDL_Texture *Loadtexture(const char *filepath, SDL_Renderer *ren);
+    static SDL_Texture* LoadtextureFromText (const char* textToRender, SDL_Color textColor, int textSize, SDL_Renderer* ren);
+    ~TextureManager();
 };
